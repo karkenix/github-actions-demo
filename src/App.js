65d097fdfd5a56403b6b0e9fc44ff74d822fc97e
karkenix <74529971+karkenix@users.noreply.github.com>
2024-12-31 16:41:01 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          <code>My test gh-pages</code>
+          My test gh-pages.
         </p>
         <a
           className="App-link"
